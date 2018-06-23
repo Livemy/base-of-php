92ebb0a5fae29262170851778c8421f9b10b25a3
lanxue-of-33 <1924826058@qq.com>
2018-06-23 15:42:10 +0800

    how to use Ajax
    这里是利用原生js实现Ajax的一些基本应用

---
+++ Ajaxsandbox/process.php
@@ -0,0 +1,31 @@
+﻿<?php
+	//给Ajax请求返回的内容
+	// echo '这里给php自制接口给Ajax请求返回的内容';
+	// 
+	if(isset($_GET['name'])){
+		echo "GET: 你的名字是：".$_GET['name'];
+	}
+	
+	//链接数据库
+	$link = mysqli_connect('localhost','root','357159','user');
+	mysqli_set_charset($link,'utf8'); //设定字符集  
+	if (!$link) {
+	 die("连接失败:".mysqli_connect_error());
+	}
+
+	// if(isset($_POST['name'])){
+	// 	echo "POST: 你的名字是：".$_POST['name'];
+	// }
+	if(isset($_POST['name'])){
+
+		//将拿到的数据转化一下,防止出现乱码的问题
+		$name=mysqli_real_escape_string($link,$_POST['name']);
+
+		$query="insert into user(username) values ('$name');";
+		if(mysqli_query($link,$query)){
+			echo '用户添加成功';
+		}else{
+			echo '添加失败: '.mysqli_error($conn);
+		}
+	}
+?>
---
+++ Ajaxsandbox/users.php
@@ -0,0 +1,39 @@
+﻿<?php
+	
+	//链接数据库
+	$link = mysqli_connect('localhost','root','357159','user');
+	mysqli_set_charset($link,'utf8'); //设定字符集  
+	if (!$link) {
+	 die("连接失败:".mysqli_connect_error());
+	}
+
+
+	$query="select * from user ";
+	$result=mysqli_query($link,$query);
+
+	//mysqli_fetch_array()只能返回结果集的一行，要想全部输出的话就要用循环
+	// $users=mysqli_fetch_array($result,MYSQLI_ASSOC);
+	// $users=mysqli_fetch_array($result);
+	// while ($users=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+	// 	echo $users['username'];
+	// }
+	/**
+	* 
+	*/
+	class User
+	{
+		
+		public $id;
+		public $username;
+	}
+	$data=array();
+	while ( $users=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+		$user=new User();
+		$user->id=$users['id'];
+		$user->username=$users['username'];
+		$data[]=$user;
+	}
+
+
+	echo json_encode($data);
+?>  
